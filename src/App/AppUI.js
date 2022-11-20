62b18e5556460a9f9f07c805a3aace3fd327b8b4
arnol.meneses <arnol.meneses@siesa.com>
2022-11-19 23:28:43 -0500

    listo para deploy

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -1,43 +1,53 @@
 import React from 'react';
+import { TodoContext } from '../TodoContext';
 import { TodoCounter } from '../TodoCounter';
 import { TodoSearch } from '../TodoSearch';
 import { TodoList } from '../TodoList';
 import { TodoItem } from '../TodoItem';
 import { CreateTodoButton } from '../CreateTodoButton';
+import { Modal } from '../Modal';
+import { TodoForm } from '../TodoForm';
 
-function AppUI({
-  totalTodos,
-  completedTodos,
-  searchValue,
-  setSearchValue,
-  searchedTodos,
-  completeTodo,
-  deleteTodo,
-}) {
+
+function AppUI() {
+
+    const {
+        error, 
+        loading, 
+        searchedTodos, 
+        completeTodo, 
+        deleteTodo,
+        openModal,
+        setOpenModal
+    } = React.useContext(TodoContext);
+
   return (
     <React.Fragment>
-      <TodoCounter
-        total={totalTodos}
-        completed={completedTodos}
-      />
-      <TodoSearch
-        searchValue={searchValue}
-        setSearchValue={setSearchValue}
-      />
+        <TodoCounter/>
+        <TodoSearch/>
+        <TodoList>
+            {error && <p>Error!</p>}
+            {loading && <p>Cargando...</p>}
+            {(!loading && !searchedTodos.length) && <p>Crea tu primer TODO</p>}
 
-      <TodoList>
-        {searchedTodos.map(todo => (
-          <TodoItem
-            key={todo.text}
-            text={todo.text}
-            completed={todo.completed}
-            onComplete={() => completeTodo(todo.text)}
-            onDelete={() => deleteTodo(todo.text)}
-          />
-        ))}
-      </TodoList>
-
-      <CreateTodoButton />
+            {searchedTodos.map(todo => (
+                <TodoItem
+                key={todo.text}
+                text={todo.text}
+                completed={todo.completed}
+                onComplete={() => completeTodo(todo.text)}
+                onDelete={() => deleteTodo(todo.text)}
+                />
+            ))}
+        </TodoList>
+        {!!openModal && (
+          <Modal>
+            <TodoForm/>
+          </Modal>
+        )}
+      <CreateTodoButton
+        setOpenModal={setOpenModal}
+      />
     </React.Fragment>
   );
 }
--- src/CreateTodoButton/index.js
+++ src/CreateTodoButton/index.js
@@ -3,14 +3,14 @@ import './CreateTodoButton.css';
 
 function CreateTodoButton(props) {
 
-    const onClickButton = (msg) => {
-        alert(msg);
+    const onClickButton = () => {
+        props.setOpenModal(prevState => !prevState);
     }
 
     return (
         <button 
             className="CreateTodoButton"
-            onClick={() => onClickButton("Aqui va la logica para crear un nuevo TODO")}
+            onClick={() => onClickButton()}
         >
             +
         </button>
